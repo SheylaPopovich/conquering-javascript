43de1df3861ec58061c49da663611ca040e84509
Sheyla <Sheylapopovich@gmail.com>
2022-02-22 13:07:14 -0700

    created a script file for day greetings

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+//new Date is a method
+var today = new Date();
+
+//getHours is an established method -Gets the hours in a date, using local time.
+var hourNow = today.getHours();
+
+var greeting; 
+
+//if else statement below:
+if (hourNow > 18) {
+    greeting = 'Good Evening!';
+}   else if (hourNow > 12) {
+    greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h2>' + greeting + '</h3>');
