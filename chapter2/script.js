8b4d6a879eb92c260f5c573eb89244c925ed054a
Sheyla <Sheylapopovich@gmail.com>
2022-02-22 19:07:04 -0700

    created variables in script js to hold detail info about the sign

--- chapter2/script.js
+++ chapter2/script.js
@@ -1,6 +1,21 @@
 console.log("This is linked!");
 
+//variable for welcome message
 var greeting = 'Hola';
 var name = 'Sheyla';
 var message = ', please check your order:';
+
+//example of a concatenation
 var welcome = greeting + name + message;
+
+
+//variables to hold detail info about the sign
+var sign = 'The Popovich home';
+var tiles = sign.length; //this is going to show a total number of ltrs in the signage
+var subTotal = tiles * 5; // this is going to take the length of tiles and multiply by 5
+var shipping = 7; 
+var grandTotal = subTotal + shipping // this line will calculate the total of shipping and subtotal
+
+
+
+
