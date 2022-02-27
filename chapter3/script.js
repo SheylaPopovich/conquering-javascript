34a53015ec652aa28cbca3adf92060cbc6f80488
Sheyla <Sheylapopovich@gmail.com>
2022-02-27 13:27:29 -0700

    inserted comments to set up structure for script file

---
+++ chapter3/script.js
@@ -0,0 +1,9 @@
+// The script is placed inside an immediately invoked function expression which helps protect the scope of variables
+
+(function ()
+
+//PART ONE: CREATE HOTEL OBJECT AND WRITE OUT THE OFFER DETAILS
+
+//Create a hotel object using object literal syntax
+
+)
