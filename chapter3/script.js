77e17a1b0e4759c3c216f7438dbe6f08e46d96f0
Sheyla <Sheylapopovich@gmail.com>
2022-02-27 13:36:06 -0700

    declared variable for hotelName, roomRate, specialRate

--- chapter3/script.js
+++ chapter3/script.js
@@ -1,9 +1,20 @@
 // The script is placed inside an immediately invoked function expression which helps protect the scope of variables
 
-(function ()
+(function (){
 
 //PART ONE: CREATE HOTEL OBJECT AND WRITE OUT THE OFFER DETAILS
 
 //Create a hotel object using object literal syntax
+let hotel = {
+name: 'Park',
+roomRate: 240, //amount in dollars
+discount: 15, //percentage discount
+offerPrice:  function() {
+    var offerRate = this.roomRate * ((100 - this.discount) / 100);
+    return offerRate;
+}
+};
 
-)
+//Write out the hotel name, standard rate, and the special rate
+let hotelName, roomRate, specialRate //declare variables
+
