f2088090470576b7229cdab05da861c1f7448b26
Sheyla <Sheylapopovich@gmail.com>
2022-02-27 13:20:08 -0700

    created a chapter three folder with an index html

--- chapter2/script.js
+++ chapter2/script.js
@@ -1,48 +1,42 @@
 console.log("This is linked!");
 
 //variable for welcome message
-var greeting = 'Hola ';
-var name = 'Sheyla';
-var message = ', please check your order:';
+var greeting = "Hola ";
+var name = "Sheyla";
+var message = ", please check your order:";
 
 //example of a concatenation
 var welcome = greeting + name + message;
 
-
 //variables to hold detail info about the sign
-var sign = 'The Popovich home';
+var sign = "The Popovich home";
 var tiles = sign.length; //this is going to show a total number of ltrs in the signage
 var subTotal = tiles * 5; // this is going to take the length of tiles and multiply by 5
-var shipping = 7; 
-var grandTotal = subTotal + shipping // this line will calculate the total of shipping and subtotal
+var shipping = 7;
+var grandTotal = subTotal + shipping; // this line will calculate the total of shipping and subtotal
 
 //get the element that has an id of greeting
-var el = document.getElementById('greeting');
+var el = document.getElementById("greeting");
 
 //replace the content of the element with the personalized welcome message
-el.textContent = welcome
+el.textContent = welcome;
 
 //get the element that has an id of userSIgn then update its contents
-var elSign = document.getElementById('userSign');
+var elSign = document.getElementById("userSign");
 elSign.textContent = sign;
 
 //get the element that has an id of tiles then update it contents
-var elTiles = document.getElementById('tiles');
+var elTiles = document.getElementById("tiles");
 elTiles.textContent = tiles;
 
 //get the element that has an id of subTotal then update its contents
-var elSubTotal = document.getElementById('subTotal');
-elSubTotal.textContent = '$' + subTotal;
+var elSubTotal = document.getElementById("subTotal");
+elSubTotal.textContent = "$" + subTotal;
 
 //get the element that has an id of shipping then update its contents
-var elShipping = document.getElementById('shipping');
-elShipping.textContent = '$' + shipping;
+var elShipping = document.getElementById("shipping");
+elShipping.textContent = "$" + shipping;
 
 //get the element that has an id of grandTotal then update its contents
-var elGrandTotal = document.getElementById('grandTotal');
-elGrandTotal.textContent = '$' + grandTotal; 
-
-
-
-
-
+var elGrandTotal = document.getElementById("grandTotal");
+elGrandTotal.textContent = "$" + grandTotal;
