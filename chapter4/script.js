d657beb718af8553a59186c3d9115b213bb2bf2e
Sheyla <Sheylapopovich@gmail.com>
2022-03-05 16:10:42 -0700

    added to else to if statement

--- chapter4/script.js
+++ chapter4/script.js
@@ -65,12 +65,14 @@
 // el.textContent = msg;
 
 //-------------------------------------------//
-var score = 75; // Score 
+var score = 95; // Score 
 var msg; // Message
 
 if (score >= 50) { //if score is 50 or higher
     msg = 'Congratulations!';
     msg += ' Proceed to the next round.'; 
+} else {
+    msg = 'Failed, try Again'
 }
 
 var el = document.getElementById('answer');
