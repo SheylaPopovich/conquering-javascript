49056161a6d5840639a1922183dc981305333250
Sheyla <Sheylapopovich@gmail.com>
2022-03-05 13:15:15 -0700

    created vars to store scores

--- chapter4/script.js
+++ chapter4/script.js
@@ -1,11 +1,22 @@
 // console.log('This is Linked');
 
-var pass = 50;  // Pass Mark
-var score = 90; // Score
+// var pass = 50;  // Pass Mark
+// var score = 90; // Score
 
 //Check if the user has passed
-var hasPassed = score >= pass;
+// var hasPassed = score >= pass;
 
 //Write the message into the page
-var el = document.getElementById('answer');
-el.textContent = 'Level Passed: ' + hasPassed;
+// var el = document.getElementById('answer');
+// el.textContent = 'Level Passed: ' + hasPassed;
+
+
+
+var score1 = 90; //Round 1 score
+var score2 = 95; //Round 2 score
+
+var highScore1 = 75; //Round 1 high score
+var highScore2 = 95; //Round 2 high score
+
+
+
