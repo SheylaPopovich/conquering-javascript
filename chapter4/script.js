223b94d18439be2baca0cbe90a10906615bc8cfc
Sheyla <Sheylapopovich@gmail.com>
2022-03-05 14:59:32 -0700

    created comparison and with two variables

--- chapter4/script.js
+++ chapter4/script.js
@@ -34,7 +34,9 @@ var pass1 = 6; //Round 1 pass mark
 var pass2 = 6; //Round 2 pass mark
 
 
+//Check whether user passed both rounds, store result in a variable
 
+var passBoth = (score1 >= pass1) && (score2 >= pass2);
 
 
 
