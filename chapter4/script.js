994b31253f170177d89f97960dde552ca5ac3c41
Sheyla <Sheylapopovich@gmail.com>
2022-03-06 12:40:11 -0700

    created a do while loop function

--- chapter4/script.js
+++ chapter4/script.js
@@ -128,12 +128,29 @@
 
 //-------------------------------------------//
 //USING WHILE LOOPS
-var i = 1;   //Set counter to 1
-var msg = '';  //Message
+// var i = 1;   //Set counter to 1
+// var msg = '';  //Message
 
 //Store 5 times table in a variable
-while (i < 10 ) {
-    msg += i + ' x5 = ' + (i * 5) + '<br />';
-    i++
-}
+// while (i < 10 ) {
+//     msg += i + ' x5 = ' + (i * 5) + '<br />';
+//     i++
+// }
+// document.getElementById('answer').innerHTML = msg;
+
+
+
+//-------------------------------------------//
+//USING DO WHILE LOOPS
+var i = 1;   //Set Counter to 1
+var msg = ' ';   ///Message
+
+
+//Store 5 times table in a variable
+do {
+    msg += i + ' x 5 = ' + (i * 5) + '<br />';
+    i++;
+} while (i < 1);
+//Note how this is already 1 and it still runs
+
 document.getElementById('answer').innerHTML = msg;
