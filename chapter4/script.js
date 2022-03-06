8845e132b2f5bfb91a400c43cb90fb1a0a9ef6a8
Sheyla <Sheylapopovich@gmail.com>
2022-03-06 16:08:14 -0700

    created a chapter 5 folder and an index file

--- chapter4/script.js
+++ chapter4/script.js
@@ -142,15 +142,15 @@
 
 //-------------------------------------------//
 //USING DO WHILE LOOPS
-var i = 1;   //Set Counter to 1
-var msg = ' ';   ///Message
+// var i = 1;   //Set Counter to 1
+// var msg = ' ';   ///Message
 
 
 //Store 5 times table in a variable
-do {
-    msg += i + ' x 5 = ' + (i * 5) + '<br />';
-    i++;
-} while (i < 1);
+// do {
+//     msg += i + ' x 5 = ' + (i * 5) + '<br />';
+//     i++;
+// } while (i < 1);
 //Note how this is already 1 and it still runs
 
-document.getElementById('answer').innerHTML = msg;
+// document.getElementById('answer').innerHTML = msg;
