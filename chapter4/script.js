c86a0fadcd1f69e936ce8e5ed06770a91eb0ffbc
Sheyla <Sheylapopovich@gmail.com>
2022-03-06 11:44:46 -0700

    inserted a for loop to pass through loop

--- chapter4/script.js
+++ chapter4/script.js
@@ -111,3 +111,19 @@ var roundNumber = 0; // Current round
 var msg = '' // message
 var i; //Counter
 
+
+//Loop through the items in the array
+for (i = 0; i < arrayLength; i++) {
+
+    //Arrays are zero based (so 0 is round 1)
+    //Add 1 to the current round
+    roundNumber = (i + 1);
+
+    //Write the current round to message
+    msg += 'Round ' + roundNumber + ': ';
+
+    //Get the Score from the scores array
+    msg += scores[i] + '<br />';
+}
+
+document.getElementById('answer').innerHTML = msg;
